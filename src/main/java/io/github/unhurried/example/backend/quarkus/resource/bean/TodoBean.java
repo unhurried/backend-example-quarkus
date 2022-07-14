ee4c824e194a38f521f6fcaf95387e4647d22fa1
unhurried <unhurried@users.noreply.github.com>
2022-07-14 22:13:54 +0900

    Add @Pattern validation rules.

--- src/main/java/io/github/unhurried/example/backend/quarkus/resource/bean/TodoBean.java
+++ src/main/java/io/github/unhurried/example/backend/quarkus/resource/bean/TodoBean.java
@@ -2,17 +2,20 @@ package io.github.unhurried.example.backend.quarkus.resource.bean;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Null;
+import javax.validation.constraints.Pattern;
 
 public class TodoBean {
     @Null
     public String id;
 
     @NotNull
+    @Pattern(regexp = "^\\p{Print}{1,32}$")
     public String title;
 
     @NotNull
     public Category category;
 
+    @Pattern(regexp = "^\\p{Print}{1,256}$")
     public String content;
 
     public enum Category {
