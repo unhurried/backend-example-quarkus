537a3a3e55ff357d61edceeb782bfae911e650e1
unhurried <unhurried@users.noreply.github.com>
2022-10-29 16:59:25 +0900

    Handle authentication errors in ExceptionMapper with Quarkus 2.14.0.CR1.

--- src/test/java/io/github/unhurried/example/backend/quarkus/HttpTest.java
+++ src/test/java/io/github/unhurried/example/backend/quarkus/HttpTest.java
@@ -1,5 +1,7 @@
 package io.github.unhurried.example.backend.quarkus;
 
+import static org.hamcrest.Matchers.equalTo;
+
 import javax.inject.Inject;
 
 import org.junit.jupiter.api.Test;
@@ -8,6 +10,7 @@ import io.github.unhurried.example.backend.quarkus.resource.TodoResource;
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.RestAssured;
+import io.restassured.filter.log.ResponseLoggingFilter;
 
 @QuarkusTest
 public class HttpTest {
@@ -20,5 +23,27 @@ public class HttpTest {
         RestAssured.given().auth().oauth2(helper.createToken())
             .when().get()
             .then().statusCode(200);
+    }
+
+    @Test
+    @TestHTTPEndpoint(TodoResource.class)
+    public void testAuthErrorNoToken() {
+        RestAssured.given()
+        .filter(new ResponseLoggingFilter())
+            .when().get()
+            .then()
+                .statusCode(401)
+                .body("error", equalTo("unauthorized"));
+    }
+
+    @Test
+    @TestHTTPEndpoint(TodoResource.class)
+    public void testAuthErrorInvalidToken() {
+        RestAssured.given().auth().oauth2("xxx")
+        .filter(new ResponseLoggingFilter())
+            .when().get()
+            .then()
+                .statusCode(401)
+                .body("error", equalTo("authentication_failed"));
     }
 }
